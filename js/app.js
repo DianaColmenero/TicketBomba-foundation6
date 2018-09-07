0c32bdfafd3e678e6b31ddafbe839f51a1dfffe8
Developer <developer@IM-M11-877.local>
2018-09-06 19:26:08 -0500

    update lineup, subscribe and information

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation()
+
+
+$('[data-toggle-dia]').click(function (ev) {
+    alert("hola");
+    console.log("panel");
+
+    const panel = $(this).data('toggleDay')
+    console.log("panel");
+    $('[ineup-tabs]').foundation('selectTab', panel)
+    
+})
